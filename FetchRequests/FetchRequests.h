5642153fc67f5714a6f0c940b824f8fcff644d4b
Adam Lickel <adam@lickel.com>
2019-06-28 15:49:02 -0700

    Initial Commit

---
+++ FetchRequests/FetchRequests.h
@@ -0,0 +1,19 @@
+//
+//  FetchRequests.h
+//  FetchRequests
+//
+//  Created by Adam Lickel on 6/28/19.
+//  Copyright © 2019 Speramus Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FetchRequests.
+FOUNDATION_EXPORT double FetchRequestsVersionNumber;
+
+//! Project version string for FetchRequests.
+FOUNDATION_EXPORT const unsigned char FetchRequestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FetchRequests/PublicHeader.h>
+
+
