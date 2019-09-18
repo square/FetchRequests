ed1b10946733c59924a2e186e72531a02e684433
Adam Lickel <adam@lickel.com>
2019-09-18 11:35:39 -0700

    Add comments

--- FetchRequests/Sources/CWFetchableObject.swift
+++ FetchRequests/Sources/CWFetchableObject.swift
@@ -8,41 +8,56 @@
 
 import Foundation
 
-public typealias CWFetchableObject = NSObject & CWFetchableObjectProtocol
-
 // As of Swift 5 this crashes
 private let canUseRawData = false
 
-// This prevents crashes on OSs bundling Swift < 5.1
+/// A class of types that should be fetchable via CWFetchRequests
+public typealias CWFetchableObject = NSObject & CWFetchableObjectProtocol
+
+/// A class of types whose instances hold the value of an entity with stable identity.
+/// This exists purely to support targetting OSes that bundle Swift before v5.1
 public protocol CWIdentifiable {
+    /// A type representing the stable identity of the entity associated with `self`.
     associatedtype ID: Hashable
-    var id: Self.ID { get }
+    /// The stable identity of the entity associated with `self`.
+    var id: ID { get }
 }
 
-public protocol _CWFetchableObjectProtocolBase: class, CWIdentifiable {
+public protocol CWRawDataRepresentable {
     #if canUseRawData
+    /// A type representing the underlying data of the entity associated with `self`.
     associatedtype RawData
     #else
     typealias RawData = [String: Any]
     #endif
 
-    var id: ID { get }
+    /// Initialize an object from raw data
+    init?(data: RawData)
+
+    /// The underlying data of the entity associated with `self`.
     var data: RawData { get }
-    var isDeleted: Bool { get }
 }
 
-public protocol CWFetchableObjectProtocol: _CWFetchableObjectProtocolBase {
-    associatedtype KeyPathBase: _CWFetchableObjectProtocolBase where
+/// A class of types that should be fetchable via CWFetchRequests
+public protocol CWFetchableObjectProtocol: class, CWRawDataRepresentable, CWIdentifiable {
+    associatedtype KeyPathBase: CWRawDataRepresentable & CWIdentifiable where
         KeyPathBase.ID == ID, KeyPathBase.RawData == RawData
 
-    init?(data: RawData)
-    var observingUpdates: Bool { get set }
+    /// Has this object been marked as deleted?
+    var isDeleted: Bool { get }
+
+    /// Parse raw data to return the expected entity ID
+    ///
+    /// - parameter data: Raw data that potentially represents a FetchableObject
+    /// - returns: The entityID for a fetchable object
+    static func entityID(from data: RawData) -> ID?
 
     static var idKeyPath: KeyPath<KeyPathBase, ID> { get }
     static var dataKeyPath: KeyPath<KeyPathBase, RawData> { get }
     static var deletedKeyPath: KeyPath<KeyPathBase, Bool> { get }
 
-    static func entityID(from data: RawData) -> ID?
+    var observingUpdates: Bool { get set }
+
     static func rawDataIsIdentical(lhs: RawData, rhs: RawData) -> Bool
 }
 
