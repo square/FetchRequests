7f3034f77365b8c0ca054d3c9af7e67de2a66b84
Adam Lickel <adam@lickel.com>
2019-11-13 18:07:02 -0800

    Add Obj-C boxed JSON

---
+++ FetchRequests/Sources/CWBoxedJSON.swift
@@ -0,0 +1,63 @@
+//
+//  CWBoxedJSON.swift
+//  FetchRequests
+//
+//  Created by Adam Lickel on 11/13/19.
+//  Copyright © 2019 Speramus Inc. All rights reserved.
+//
+
+import Foundation
+
+public class CWBoxedJSON: NSObject {
+    internal let json: CWJSON
+
+    public init(_ json: CWJSON) {
+        self.json = json
+    }
+
+    @objc
+    public var object: Any {
+        return json.object
+    }
+
+    @objc
+    public subscript(key: String) -> CWBoxedJSON? {
+        return json[key] as CWBoxedJSON?
+    }
+
+    @objc
+    public subscript(offset: Int) -> CWBoxedJSON? {
+        return json[offset] as CWBoxedJSON?
+    }
+}
+
+//swiftlint:disable identifier_name
+
+extension CWJSON: _ObjectiveCBridgeable {
+    public func _bridgeToObjectiveC() -> CWBoxedJSON {
+        return CWBoxedJSON(self)
+    }
+
+    public static func _forceBridgeFromObjectiveC(
+      _ source: CWBoxedJSON,
+      result: inout CWJSON?
+    ) {
+        result = source.json
+    }
+
+    public static func _conditionallyBridgeFromObjectiveC(
+      _ source: CWBoxedJSON,
+      result: inout CWJSON?
+    ) -> Bool {
+        result = source.json
+        return true
+    }
+
+    public static func _unconditionallyBridgeFromObjectiveC(
+        _ source: CWBoxedJSON?
+    ) -> CWJSON {
+      var result: CWJSON?
+      _forceBridgeFromObjectiveC(source!, result: &result)
+      return result!
+    }
+}
