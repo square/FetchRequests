e6230d5ee688ef4ff261a69e86ca308cb7214b2c
Adam Lickel <adam@lickel.com>
2021-09-14 16:08:18 -0700

    Update to 4.0.0

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-collections", from: "0.0.4"),
+        .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
     ],
     targets: [
         .target(
