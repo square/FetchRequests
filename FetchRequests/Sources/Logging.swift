4841cfc89e3791844d38770942709a33e9934b3b
Adam Lickel <adam@lickel.com>
2023-04-06 23:09:11 -0700

    Linter fun

--- FetchRequests/Sources/Logging.swift
+++ FetchRequests/Sources/Logging.swift
@@ -11,24 +11,124 @@ import Foundation
 #if canImport(CocoaLumberjack)
 import CocoaLumberjack
 
-func CWLogDebug(_ message: @autoclosure () -> String, level: DDLogLevel = dynamicLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = true, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogDebug(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+func CWLogVerbose(
+    _ message: @autoclosure () -> String,
+    level: DDLogLevel = dynamicLogLevel,
+    context: Int = 0,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    tag: Any? = nil,
+    asynchronous async: Bool = true,
+    ddlog: DDLog = DDLog.sharedInstance
+) {
+    DDLogVerbose(
+        message(),
+        level: level,
+        context: context,
+        file: file,
+        function: function,
+        line: line,
+        tag: tag,
+        asynchronous: async,
+        ddlog: ddlog
+    )
 }
 
-func CWLogInfo(_ message: @autoclosure () -> String, level: DDLogLevel = dynamicLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = true, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogInfo(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+func CWLogDebug(
+    _ message: @autoclosure () -> String,
+    level: DDLogLevel = dynamicLogLevel,
+    context: Int = 0,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    tag: Any? = nil,
+    asynchronous async: Bool = true,
+    ddlog: DDLog = DDLog.sharedInstance
+) {
+    DDLogDebug(
+        message(),
+        level: level,
+        context: context,
+        file: file,
+        function: function,
+        line: line,
+        tag: tag,
+        asynchronous: async,
+        ddlog: ddlog
+    )
 }
 
-func CWLogWarning(_ message: @autoclosure () -> String, level: DDLogLevel = dynamicLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = true, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogWarn(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+func CWLogInfo(
+    _ message: @autoclosure () -> String,
+    level: DDLogLevel = dynamicLogLevel,
+    context: Int = 0,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    tag: Any? = nil,
+    asynchronous async: Bool = true,
+    ddlog: DDLog = DDLog.sharedInstance
+) {
+    DDLogInfo(
+        message(),
+        level: level,
+        context: context,
+        file: file,
+        function: function,
+        line: line,
+        tag: tag,
+        asynchronous: async,
+        ddlog: ddlog
+    )
 }
 
-func CWLogVerbose(_ message: @autoclosure () -> String, level: DDLogLevel = dynamicLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = true, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogVerbose(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+func CWLogWarning(
+    _ message: @autoclosure () -> String,
+    level: DDLogLevel = dynamicLogLevel,
+    context: Int = 0,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    tag: Any? = nil,
+    asynchronous async: Bool = true,
+    ddlog: DDLog = DDLog.sharedInstance
+) {
+    DDLogWarn(
+        message(),
+        level: level,
+        context: context,
+        file: file,
+        function: function,
+        line: line,
+        tag: tag,
+        asynchronous: async,
+        ddlog: ddlog
+    )
 }
 
-func CWLogError(_ message: @autoclosure () -> String, level: DDLogLevel = dynamicLogLevel, context: Int = 0, file: StaticString = #file, function: StaticString = #function, line: UInt = #line, tag: Any? = nil, asynchronous async: Bool = false, ddlog: DDLog = DDLog.sharedInstance) {
-    DDLogError(message(), level: level, context: context, file: file, function: function, line: line, tag: tag, asynchronous: async, ddlog: ddlog)
+func CWLogError(
+    _ message: @autoclosure () -> String,
+    level: DDLogLevel = dynamicLogLevel,
+    context: Int = 0,
+    file: StaticString = #file,
+    function: StaticString = #function,
+    line: UInt = #line,
+    tag: Any? = nil,
+    asynchronous async: Bool = false,
+    ddlog: DDLog = DDLog.sharedInstance
+) {
+    DDLogError(
+        message(),
+        level: level,
+        context: context,
+        file: file,
+        function: function,
+        line: line,
+        tag: tag,
+        asynchronous: async,
+        ddlog: ddlog
+    )
 }
 #else
 func CWLogDebug(_ message: @autoclosure () -> String) {
