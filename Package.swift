7bdd040dde8ca5cf371da3899a478c42cff2d4ed
Adam Lickel <adam@lickel.com>
2024-08-30 15:05:32 -0700

    Tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.0
 
 import PackageDescription
 
@@ -36,5 +36,5 @@ let package = Package(
             path: "FetchRequests/Tests"
         ),
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageVersions: [.v6]
 )
